9d7b6538435b4cc8cf9a5c32c391eca01207c03c
KEERTHIG DEVI <sdkeerthigadevi@gmail.com>
2025-09-13 21:36:10 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,17 @@ connectDB()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials: true}))
+app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true); // allows tools like Postman
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error('Not allowed by CORS'), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true
+  }));
+  
 
 // API Endpoints
 app.get('/', (req, res) => res.send("API is Working"))
