ab6372146a2be1093d8c787dd67741e62f90fe77
KEERTHIG DEVI <sdkeerthigadevi@gmail.com>
2025-09-14 00:53:58 +0530

    fix email issue

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,6 +1,6 @@
 import axios from "../utlis/axiosInstance.js"
 
 export const userDetails = async(data) => {
-    const res = await axios.get('/api/user/data', data)
+    const res = await axios.get('/api/user/data')
     return res.data
 }
